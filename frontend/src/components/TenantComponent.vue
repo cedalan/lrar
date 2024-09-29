f1f83a30614407f0cb3fc2e9282dd0c0e4416f69
Elias Dalan <cedalan@ntnu.no>
2024-09-29 20:39:53 +0200

    Added Skeleton for TentantComponent

---
+++ frontend/src/components/TenantComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error"> {{ error }} </div>
+        <div v-else>
+            <p> {{ tenant?.name }} </p>
+            <p> {{ tenant?.img }} </p>
+            <p> {{ tenant?.burns }} </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface Tenant {
+    name: string,
+    imagePath: string,
+    burns: number,
+}
+
+export default defineComponent ({
+    name: "TenantComponent",
+    data() {
+        return {
+            tentant: null as Tenant | null,
+            loading: true,
+            error: null as string | null,
+        };
+    },
+    async created() {
+        try {
+            //Send request to backend and blah blah
+            this.tentat.name = "Mr. Test";
+            this.tenant.imagePath = "some/path/to/image";
+            this.tenant.burns = 4;
+        } catch (err: any) {
+            this.error = err.message;
+        } finally {
+            this.loading = false;
+        }
+    }
+});
+</script>
