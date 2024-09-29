11f687c8ea92c1e7ca2a5a42f1162b76f473aa86
Jonas Mørkland <jonasmorkland@outlook.com>
2024-09-29 16:20:27 +0200

    feat: make backend a server that listens on port 3001. Also create a simple endpoint that returns the greeting 'Hello, World!' to the user when a request is made to 'localhost:3001'.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    "Hello, World!\n"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    const PORT: u16 = 3001;
+    
+    println!("Listening on port {}...", PORT);
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+    .bind(format!("127.0.0.1:{}", PORT))?
+    .run()
+    .await
 }
