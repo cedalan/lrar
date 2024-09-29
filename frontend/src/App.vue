53b117e55e45df43ec603652c9ba8ec92697ef59
Lars Jaatun <lars@atilf.no>
2024-09-29 19:20:10 +0200

    move scaffold code to another folder, keep folder structure in src folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="LRAR logo" class="logo" src="@/assets/LRAR_logo.jpg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="What did you do now..." />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
