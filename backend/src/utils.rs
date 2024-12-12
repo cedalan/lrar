6f9cab560c9ec1adc049232c3ed5e57a435b2376
Elias Dalan <cedalan@ntnu.no>
2024-12-12 23:56:41 +0100

    Implemented chore rotation

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,3 +1,5 @@
+use chrono::{Local, NaiveDate, Timelike, Utc};
+
 pub fn get_weekly_chore() -> Vec<String> {
     let all_chores: Vec<String> = vec![
         "Clean the kitchen".to_string(),
@@ -8,5 +10,25 @@ pub fn get_weekly_chore() -> Vec<String> {
         "Take out all trash".to_string(),
     ];
 
-    return all_chores;
+    //Arbritrary start date
+    let start_date = NaiveDate::from_ymd_opt(2024, 12, 9).expect("Invalid date");
+    let current_date = Utc::now().naive_utc().date();
+
+    let weeks_since_start = (current_date.signed_duration_since(start_date).num_weeks()) as usize;
+
+    let num_chores = all_chores.len();
+
+    //Calculates the "rotation index"
+    let chore_index = weeks_since_start % num_chores;
+
+    // Rotate the list to the right by 'chore_index'
+    let rotated_chores: Vec<_> = all_chores
+        .iter()
+        .cycle()
+        .skip(num_chores - chore_index) // Shift to the right by the calculated index
+        .take(num_chores)
+        .cloned()
+        .collect();
+
+    return rotated_chores;
 }
