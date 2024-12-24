03d2fd4ac482f2bb15a701465989c7a37deb42ca
Jonas Mørkland <jonasmorkland@outlook.com>
2024-12-24 15:17:05 +0100

    chore: rename App_lrar.vue to App.vue.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,7 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import App from './App.vue'
 
-import App_lrar from './App_lrar.vue'
-
-const app = createApp(App_lrar)
+const app = createApp(App)
 
 app.mount('#app')
