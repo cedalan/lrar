44622ee0ca66bd1e0e29931ede03e9482a0b4b06
Jonas Mørkland <jonasmorkland@outlook.com>
2024-12-24 14:48:27 +0100

    chore: remove Pinia from project.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-//import App from './App.vue'
 import App_lrar from './App_lrar.vue'
 
 const app = createApp(App_lrar)
-
-app.use(createPinia())
 
 app.mount('#app')
