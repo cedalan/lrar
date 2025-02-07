58ea2190c9d0487b2bb9b00dac98027341d9f8c7
Elias Dalan <cedalan@ntnu.no>
2025-02-07 11:35:02 +0100

    Made new note insertable

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -12,7 +12,7 @@ pub struct Note {
     pub created_at: NaiveDateTime,
 }
 
-#[derive(Queryable, Selectable, Serialize, Debug)]
+#[derive(Insertable, Selectable, Serialize, Debug)]
 #[diesel(table_name = notes)]
 pub struct NewNote {
     pub message: String,
