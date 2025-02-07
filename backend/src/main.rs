f7fe9b040209093309d848ca5f40eb9039fb6e08
Elias Dalan <cedalan@ntnu.no>
2025-02-07 12:33:37 +0100

    Serviced new endpoints in main function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,7 +10,7 @@ mod schema;
 mod endpoints;
 mod utils;
 
-use endpoints::{create_burn, create_tenant, get_tenant_burns, get_tenants};
+use endpoints::{create_burn, create_tenant, create_note, get_notes, get_tenant_burns, get_tenants};
 
 const PORT: u16 = 3001;
 
@@ -42,8 +42,10 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(hello))
             .service(get_tenants)
             .service(get_tenant_burns)
+            .service(get_notes)
             .service(create_burn)
             .service(create_tenant)
+            .service(create_note)
     })
     .bind(format!("0.0.0.0:{}", PORT))?
     .run()
