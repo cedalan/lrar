5802ff4045419d9fe7b85469fee83f1d633caf74
Elias Dalan <cedalan@ntnu.no>
2025-03-16 13:24:42 +0100

    Added some stupid css...

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="everything">
         <h1 id="welcome-text">Welcome to LRAR - Lazy Roomate Avoidance Registry!</h1>
-        <button @click="openSchema(true)">+</button>
+        <button @click="openSchema(true)">Add Tenant</button>
     </div>
     <TenantFormComponent
       v-if="showTenantForm"
@@ -31,3 +31,28 @@ export default defineComponent({
     }
 })
 </script>
+
+<style scoped>
+#everything {
+    display: flex;
+    flex-direction: row;
+}
+
+button {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    background-color: #C2FBD7;
+    border-radius: 50px;
+    border-width: 0;
+    box-shadow: rgba(25, 25, 25, 0.04) 0 0 1px 0, rgba(0, 0, 0, 0.1) 0 3px 4px 0;
+    color: #008000;
+    cursor: pointer;
+    display: inline-block;
+    font-family: Arial, sans-serif;
+    font-size: 1em;
+    height: 50px;
+    padding: 0 25px;
+}
+</style>
