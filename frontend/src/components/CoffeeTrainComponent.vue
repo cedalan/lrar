ec8068d9c341c742d5b50fbce10a0c298568de48
Lars Andreassen Jaatun <very.lars@gmail.com>
2025-03-28 14:35:51 +0100

    MVP for coffee train, coffee train lights up if someone has pressed it!

---
+++ frontend/src/components/CoffeeTrainComponent.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, computed} from "vue";
+
+const COFFEE_TRAIN_SCHEDULED_ICON = '/src/public/coffeeTrainScheduled_small.png'
+const COFFEE_TRAIN_NOT_SCHEDULED_ICON = '/src/public/coffeeTrainUnscheduled_small.png'
+
+const coffeeTrainIsScheduled = ref(JSON.parse(localStorage.getItem('coffeeTrainIsScheduled') ?? 'false'));
+// MVP = pressing coffee train button makes happy smiley train. Next step is to make a popup and stuff to show where
+// and when the coffee train is departing from
+function toggleCoffeeTrainScheduled() {
+  coffeeTrainIsScheduled.value = !coffeeTrainIsScheduled.value;
+  localStorage.setItem('coffeeTrainIsScheduled',JSON.stringify(coffeeTrainIsScheduled.value))
+}
+
+
+const coffeeTrainIconPath = computed(() =>
+  {return coffeeTrainIsScheduled.value ? COFFEE_TRAIN_SCHEDULED_ICON : COFFEE_TRAIN_NOT_SCHEDULED_ICON}
+);
+
+</script>
+
+<template>
+<button @click="toggleCoffeeTrainScheduled"><img :src="coffeeTrainIconPath" alt="coffee train!" /></button>
+</template>
+
+<style scoped>
+
+</style>
